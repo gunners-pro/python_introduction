9fbfde698d5ea43135d6817854948c365388e1de
Fabricyo Barreto <bass.barreto@gmail.com>
2023-07-15 07:50:20 -0300

    feat: add resolution

--- iterating_through_dictionaries.py
+++ iterating_through_dictionaries.py
@@ -8,8 +8,10 @@ basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
 fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
 
 #Iterate through the dictionary
-
+for key, value in basket_items.items():
 #if the key is in the list of fruits, add the value (number of fruits) to result
+    if key in fruits:
+        result += value
 
 
 print(result)
